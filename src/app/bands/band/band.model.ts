d6d32101cc0fe6c407fb35330fa54b5405c6aee5
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2021-12-11 23:30:54 -0300

    Ajustando os dados das bandas

--- src/app/bands/band/band.model.ts
+++ src/app/bands/band/band.model.ts
@@ -1,8 +1,8 @@
 export interface Band {
-  id_banda: number;
+  idBanda: string;
   avaliacao: number;
   nome: string;
-  lider: string;
-  genero: string;
-  image_path: string;
+  origem: string;
+  generos: string[];
+  imagePath: string;
 }
